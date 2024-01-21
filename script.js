9998690130c447d6ea8348d44a45c7cc24a76542
Dhruv Patel <122099280+dhruvurgr8@users.noreply.github.com>
2024-01-21 18:09:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+function foucusNext(inp){
+	const nextInp = inp.nextElementSibling;
+	if(nextInp){
+		nextInp.focus();
+	}
+}
+function focusPrevious(input) {
+      const previousInput = input.previousElementSibling;
+      if (event.key === 'Backspace' && previousInput) {
+        previousInput.focus();
+      }
+    }
